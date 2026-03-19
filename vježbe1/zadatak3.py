d58e0a667f252c5426ae80ef8f85210e4ce0508e
Ema Matkovic <ematkovic@pmfst.hr>
2026-03-19 13:20:23 +0100

    treći zadatak

---
+++ vježbe1/zadatak3.py
@@ -0,0 +1,6 @@
+x1 = float(input("Unesi koordinatu x za prvu točku: "))
+y1 = float(input("Unesi koordinatu y za prvu točku: "))
+x2 = float(input("Unesi koordinatu x za drugu točku: "))
+y2 = float(input("Unesi koordinatu y za drugu točku: "))
+
+print(f"y-{y1}=({y2}-{y1})/({x2}-{x1})*(x-{x1})")
